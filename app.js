0a8ed230e3b0d98f07d05cac111c6d4aa726d1b6
Seu Nome <seuemail@exemplo.com>
2025-01-28 14:19:44 -0300

    modificacoes fora da aula

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está conectado ao banco de dados")
+
+// mais um console de teste 
+console.log("Eu fiz mais um teste fora da aula")
